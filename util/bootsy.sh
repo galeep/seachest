dca7adac790cd96cfa2dbe30eb8179eb7257e4cb
Gale Pedowitz Fagan <galeep@users.noreply.github.com>
2017-11-11 20:28:26 -0800

    Script to add utilities to a vanilla Ubuntu 14.04 VM

---
+++ util/bootsy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Bootstrap basic box
+# curl things that curl other things :( 
+
+sudo apt-get update
+sudo apt-get upgrade
+
+function agi { 
+    sudo apt-get install -y ${1}
+}
+
+function hashiget() { 
+    _scriptfile=${1}
+    _product=${2}
+    _vers=${3}
+    sudo ${_scriptfile} -n ${_product} -a linux_amd64 -v ${_vers} -b profile 
+}
+
+agi unzip
+agi golang
+
+GIST_URL="https://gist.githubusercontent.com/galeep/4e00cb262d73674e787444bd63714080/raw/488fb75c16e4500ba10218291226cdc376511261/install_hashicorp_app.sh"
+
+scriptlet="pootie"
+scriptname="${scriptlet}-`date "+%s"`"
+
+# chop it up to simplify things 
+scriptbase=`basename ${scriptname}`
+scriptdir=`dirname ${scriptname}`
+scriptfile="${scriptdir}/${scriptbase}"
+
+curl -s -o ${scriptfile} $GIST_URL
+chmod 755 ${scriptfile}
+
+hashiget ${scriptfile} packer 1.1.1
+hashiget ${scriptfile} terraform 0.10.8
